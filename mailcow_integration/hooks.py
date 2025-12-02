480a28d6c26e590d99b61f60f03fba843ef4a8fa
Günther Eder <guenther.eder@indigitalform.com>
2025-12-02 19:27:29 +0100

    -doctype mailcow settings added -hooks updated

--- mailcow_integration/hooks.py
+++ mailcow_integration/hooks.py
@@ -1,5 +1,5 @@
 doc_events = {
     "User": {
-        "after_insert": "mailcow_integration.mailcow_api.user_hooks.create_mailcow_mailbox"
+        "after_insert": "mailcow_integration.user_hooks.create_mailcow_mailbox"
     }
 }
