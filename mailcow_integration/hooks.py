4157192ed1b80733776273fcc26bf135fc6d1c34
Günther Eder <guenther.eder@indigitalform.com>
2025-12-02 19:54:28 +0100

    -trying to improve hooks.py

--- mailcow_integration/hooks.py
+++ mailcow_integration/hooks.py
@@ -1,3 +1,10 @@
+app_name = "mailcow_integration"
+app_title = "Mailcow Integration"
+app_publisher = "In Digital Form GmbH"
+app_description = "Mailcow integration for ERPNext"
+app_email = "guenther.eder@indigitalform.com"
+app_license = "mit"
+
 doc_events = {
     "User": {
         "after_insert": "mailcow_integration.user_hooks.create_mailcow_mailbox"
